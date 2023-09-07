7c249bb7d4670efb4a9ff52e131bdf80ec2f6b12
Victor <vicky.robilson5528@gmail.com>
2023-09-07 16:05:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-* main - A program that print a puts function
-* Return: 0 (Success)
+ * main - A program that print a puts function
+ * Return: 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzz
-le");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
