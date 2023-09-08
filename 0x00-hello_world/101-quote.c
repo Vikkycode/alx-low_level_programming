49a07f48af5c3d910a85620d0292ce82bd06028b
Victor <vicky.robilson5528@gmail.com>
2023-09-07 17:15:12 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program that prints put and prints line to the standard error
- * Return 1 (Success)
+ * Return:1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
