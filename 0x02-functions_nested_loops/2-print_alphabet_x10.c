042728af64eab014e371533edfc9ed8ca19bef6e
Victor <vicky.robilson5528@gmail.com>
2023-09-13 02:13:04 -0700

    2 print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that print alphabet x10
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	_putchar('\n');
+}
