27213eaa22847d63e2d95b1e19aaf44badd8aca6
Victor <vicky.robilson5528@gmail.com>
2023-09-13 02:29:43 -0700

    2 print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,10 @@ void print_alphabet_x10(void)
 	char i;
 	char j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-		{
 			_putchar(j);
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
