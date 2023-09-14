03e98bfe82d215c2d8b986b1fbc3d7261a24e9e9
Victor <vicky.robilson5528@gmail.com>
2023-09-13 21:54:28 -0700

    101 natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,14 +10,15 @@ int main(void)
 	int sum = 0;
 	int i;
 
-	for (i = 0; i < 1024; i++)
+	while ( i < 1024)
 	{
-		if (i % 3 == 0 || 1 % 5 == 0)
-			sum = sum + 1;
+	if (( i % 3 == 0) || ( i % 5 == 0))
+	{
+	sum += i;
 	}
-
+	i++;
+	}
 	printf("%d\n", sum);
-
 	return (0);
 }
 
