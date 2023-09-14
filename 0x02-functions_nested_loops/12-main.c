6ad1e835aa43067a0b2fc7302ceb9287acac8d8b
Victor <vicky.robilson5528@gmail.com>
2023-09-13 20:30:08 -0700

    1100 times table

--- 0x02-functions_nested_loops/12-main.c
+++ 0x02-functions_nested_loops/12-main.c
@@ -1,5 +1,4 @@
 #include  "main.h"
-#include <stdio.h>
 /**
  * main - check the code 
  *
@@ -11,7 +10,7 @@ int main(void)
 	print_times_table(3);
 	_putchar('\n');
 	print_times_table(5);
-	-putchar('\n');
+	_putchar('\n');
 	print_times_table(98);
 	_putchar('\n');
 	print_times_table(12);
