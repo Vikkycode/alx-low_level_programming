ff96f13b49ce6a91ea6dedcc16cb562ceb5b980b
Victor <vicky.robilson5528@gmail.com>
2023-09-15 02:45:46 -0700

    101 print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,21 +2,21 @@
 
 /**
  * print_number - prints an integer
- * @n - integer to be printed
+ * @n: integer to be printed
  */
 
 void print_number(int n)
 {
 	unsigned int n1;
 
-	if (n > 0)
+	if (n < 0)
 	{
 		n1 = -n;
 		_putchar('-');
 	}
 	else
 	{
-		n1=n;
+		n1 = n;
 	}
 
 	if (n1 / 10)
@@ -24,5 +24,4 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
-} 
-	
+}
