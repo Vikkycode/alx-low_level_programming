a3871940c0b4193a9360c02b6df2457fd3ae524e
Victor <vicky.robilson5528@gmail.com>
2023-09-15 06:31:09 -0700

    7 print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,27 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
-		int i, j;
 
-		for (i = 0; i <= n; i++)
+
+
+	int i; 
+	int j;
+
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
 		{
-			for (j = 0; j < n; j++)
+			if (j == i)
 			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
+				_putchar('\\');
+			} else
+			{
+				_putchar(' ');
 			}
-			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
