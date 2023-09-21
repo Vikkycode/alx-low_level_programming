2dddea125be47caee1949ffba65b42183536551e
Victor <vicky.robilson5528@gmail.com>
2023-09-21 07:04:17 -0700

    102 magic

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - Write a function that prints an integer.
+ *@n: n parameter
+ *Return - always 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	putchar((n1 % 10) + '0');
+}
