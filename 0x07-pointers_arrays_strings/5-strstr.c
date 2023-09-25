2e56217197ec3f7b106fca9ec05afa208a7dd6c9
Victor <vicky.robilson5528@gmail.com>
2023-09-25 08:37:46 -0700

    7 print chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include  "main.h"
+
+/**
+ * *_strstr - Write a function that locates a substring
+ * @haystack: haystack point to a char
+ * @needle: needle point to a char
+ * Return:
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
