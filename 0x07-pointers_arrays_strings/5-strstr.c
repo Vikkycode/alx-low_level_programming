de90675582f047bb6a246f3d2f44b02414f1e151
Victor <vicky.robilson5528@gmail.com>
2023-09-25 20:37:57 -0700

    5 strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,9 +4,29 @@
  * *_strstr - Write a function that locates a substring
  * @haystack: haystack point to a char
  * @needle: needle point to a char
- * Return:
+ * Return:0
  */
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
 
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			}
+		while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
