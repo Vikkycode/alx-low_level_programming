3c95461a0c0547c26ea31a67dd3a5c16e980f87a
Victor <vicky.robilson5528@gmail.com>
2023-09-28 07:24:26 -0700

    6 is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * prime_a - detect to find prima
+ * @a: a paramater
+ * @b: b paramater
+ * Return: a and b
+ */
+int prime_a(int a, int b)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime_a(a, b + 1));
+}
+/**
+ * is_prime_number - detect if it is a prime no
+ *@n: the input number
+ *Return: 0 or 1
+*/
+
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
