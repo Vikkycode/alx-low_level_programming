f5bdbdfed1fff8ad88cc32dfb76969287eb5fa8c
Victor <vicky.robilson5528@gmail.com>
2023-10-02 14:57:46 -0700

    3 multiple numbr

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,76 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - function atoi
+ * @s: string to be convert
+ *
+ * Return: Always 0(Success)
+ */
+int _atoi(char *s)
+{
+	int i;
+	int j;
+	int num;
+	int len;
+	int f;
+	int digit;
+
+	i = 0;
+	j = 0;
+	num = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++j;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (j % 2)
+				digit = -digit;
+			num = num * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		j++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (num);
+}
+
+/**
+ * main - multiples two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
