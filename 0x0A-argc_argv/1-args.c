159f58997debcdd5f04ed0330767fa77317b10dc
Victor <vicky.robilson5528@gmail.com>
2023-10-02 14:05:30 -0700

    1 args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - this is the entry point of a program
+ * @argc: number of argument
+ * @argv: arraY of array
+ * Return: Always 0(Success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
