f5f91f62a65b44179270236039612f070e2c0c7c
Victor <vicky.robilson5528@gmail.com>
2023-11-03 05:16:27 -0700

    1 create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file_d = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_d == -1)
 		return (-1);
 
-	if (!text_context)
+	if (!text_content)
 		text_content = "";
-	for (nLetters = 0; text_content[nLetters]; nLetter++)
+	for (nLetters = 0; text_content[nLetters]; nLetters++)
 
 		rwr = write(file_d, text_content, nLetters);
 	if (rwr == -1)
@@ -27,3 +27,4 @@ int create_file(const char *filename, char *text_content)
 
 	close(file_d);
 	return (1);
+}
