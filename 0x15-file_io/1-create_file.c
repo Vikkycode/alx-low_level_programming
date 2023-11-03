c8160f3459e76b50ac826a946b6d667a3eb0496d
Victor <vicky.robilson5528@gmail.com>
2023-11-03 05:11:45 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - function that create a file
+ * @filename:filename pointer variable
+ * @text_content: text_content pointer variable
+ * Return: 1 or -1
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	int nLetters, file_d, rwr;
+
+	if (!filename)
+		return (-1);
+	file_d = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
+	if (file_d == -1)
+		return (-1);
+
+	if (!text_context)
+		text_content = "";
+	for (nLetters = 0; text_content[nLetters]; nLetter++)
+
+		rwr = write(file_d, text_content, nLetters);
+	if (rwr == -1)
+		return (-1);
+
+	close(file_d);
+	return (1);
