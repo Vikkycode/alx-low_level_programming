6ed89e88d145b4ee2ddcab00d6e2b22761f9e78a
Victor <vicky.robilson5528@gmail.com>
2023-11-03 04:34:20 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,43 @@
 #include "main.h"
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
-
 /**
  * read_textfile - function that read from a file that print to POSIX
  * @filename: pointer filename variable
  * @letters: letters variable
  * Return: lenw
  **/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t lenread;
-	ssize_t lenwrite;
-	int file_d;
-	char *buffer;
+	ssize_t lenread, lenwrite;
+	FILE *file;
+	char *butter[letters];
 
 	if (filename == NULL)
 	{
-		return (0);
+	return (0);
 	}
-	file_d = open(filename, O_RDONLY);
-	if (file_d == -1)
+
+	file = fopen(filename, "r");
+
+	if (file == NULL)
 	{
 		return (0);
 	}
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	lenread = fread(buffer, 1, letters, file);
+
+	fclose(file);
+
+	if (lenread < 0)
 	{
-		close(file_d);
 		return (0);
 	}
-	lenread = read(file_d, buffer, letters);
-	close(file_d);
-	if (lenread == -1)
+
+	lenwrite = fwrite(buffer, 1, lenr, stdout);
+
+	if (lenwrite < 0 || lenread != lenwrite)
 	{
-		free(buffer);
 		return (0);
 	}
-	lenwrite = write(STDOUT_FILENO, buffer, lenread);
-	free(buffer);
-	if (lenread != lenwrite)
-		return (0);
 	return (lenwrite);
 }
