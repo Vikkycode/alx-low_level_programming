a704f78bffea520077a5a5ec3e7a485cd2bc96da
Victor <vicky.robilson5528@gmail.com>
2023-11-03 04:00:23 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,42 @@
-#include <stdio.h>
 #include "main.h"
 #include <unistd.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 /**
- * read_textfile - function that read text from a file that print to POSIX
- *@filename: file pointer variable
- *@letters: letters variable
+ * read_textfile - function that read from a file that print to POSIX
+ * @filename: pointer filename variable
+ * @letters: letters variable
  * Return: lenw
-**/
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buffer[letters];
-	FILE *file = fopen(filename, "r");
+	ssize_t lengthr, lengthw;
+	char *buffer;
+	int file_d;
 
-	if (file == NULL)
+	if (filename == NULL)
+		return (0);
+	file_d = open(filename, O_RDONLY);
+	if (file_d == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
+		close(file_d);
 		return (0);
 	}
-	ssize_t lenr = fread(buffer, 1, letters, file);
-
-	fclose(file);
-
-	if (lenr < 0)
+	lengthr = read(file_d, buffer, letters);
+	close(file_d);
+	if (lengthr == -1)
 	{
+		free(buffer);
 		return (0);
 	}
-
-	ssize_t lenw = fwrite(buffer, 1, lenr, stdout);
-
-	if (lenw < 0 || lenr != lenw)
-	{
+	lengthw = write(STDOUT_FILENO, buffer, lengthr);
+	free(buffer);
+	if (lengthr != lengthw)
 		return (0);
-	}
-	return (lenw);
+	return (lengthw);
 }
